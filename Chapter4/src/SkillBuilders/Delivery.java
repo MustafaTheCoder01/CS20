3809122941a3dea8b098c98982b66b3de020b455
510139008 <510139008@L815WD220035.edu.cbe.ab.ca>
2026-09-23 11:42:08 -0600

    11:41AM

---
+++ Chapter4/src/SkillBuilders/Delivery.java
@@ -0,0 +1,41 @@
+package SkillBuilders;
+
+import java.util.Scanner;
+
+public class Delivery {
+
+	public static void main(String[] args) {
+		
+		int length;
+        int width;
+	    int height;
+	
+	    Scanner input = new Scanner(System.in);
+	    
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+
+}
---
+++ Chapter4/src/SkillBuilders/Hurricane.java
@@ -0,0 +1,59 @@
+package SkillBuilders;
+
+import java.util.Scanner;
+
+public class Hurricane {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+        int category;
+
+        System.out.print("Enter the hurricane category (1-5): ");
+        category = input.nextInt();
+
+        if (category == 1)
+        {
+            System.out.println("Category 1 Hurricane");
+            System.out.println("Wind Speed: 74-95 mph");
+            System.out.println("Wind Speed: 64-82 kts");
+            System.out.println("Wind Speed: 119-153 km/hr");
+        }
+        else if (category == 2)
+        {
+            System.out.println("Category 2 Hurricane");
+            System.out.println("Wind Speed: 96-110 mph");
+            System.out.println("Wind Speed: 83-95 kts");
+            System.out.println("Wind Speed: 154-177 km/hr");
+        }
+        else if (category == 3)
+        {
+            System.out.println("Category 3 Hurricane");
+            System.out.println("Wind Speed: 111-130 mph");
+            System.out.println("Wind Speed: 96-113 kts");
+            System.out.println("Wind Speed: 178-209 km/hr");
+        }
+        else if (category == 4)
+        {
+            System.out.println("Category 4 Hurricane");
+            System.out.println("Wind Speed: 131-155 mph");
+            System.out.println("Wind Speed: 114-135 kts");
+            System.out.println("Wind Speed: 210-249 km/hr");
+        }
+        else if (category == 5)
+        {
+            System.out.println("Category 5 Hurricane");
+            System.out.println("Wind Speed: Greater than 155 mph");
+            System.out.println("Wind Speed: Greater than 135 kts");
+            System.out.println("Wind Speed: Greater than 249 km/hr");
+        }
+        else
+        {
+            System.out.println("Invalid category. Please enter a number from 1 to 5.");
+        }
+
+        input.close();
+
+	}
+
+}
